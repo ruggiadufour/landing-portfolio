a65296f56c41edb5247288dd57789d19aff40ac8
Ruggia <angelruggia@gmail.com>
2020-12-20 17:52:30 -0300

    Added contacting ways

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+//Events--------------------------------------------------------------------------------------------
+function copyEmail() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    //Show the notification element
+    var notif = document.getElementById("Notification");
+    notif.style.display = "block"
+}
+function closeNotif(){
+    //Close the notification element
+    var notif = document.getElementById("Notification");
+    notif.style.display = "none"
+}
+
+//Scrollreveal-------------------------------------------------------------------------------------
 window.sr = ScrollReveal();
 
 sr.reveal(".navbar",{
@@ -15,6 +38,12 @@ sr.reveal("h2",{
 sr.reveal("h1",{
     duration: 3000,
     origin: "right",
+    distance: "-200px",
+    reset: true
+});
+sr.reveal("p",{
+    duration: 3000,
+    origin: "left",
     distance: "-200px",
     reset: true
 });
