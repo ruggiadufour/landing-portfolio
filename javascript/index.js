2dead99db9c2b81c4d4437989b09c89b1983efb1
angel ruggia <angelruggia@gmail.com>
2021-02-08 14:52:31 -0300

    Scroll reveal correction

--- javascript/index.js
+++ javascript/index.js
@@ -26,27 +26,21 @@ window.sr = ScrollReveal();
 sr.reveal(".navbar",{
     duration: 3000
 });
-sr.reveal("h2",{
+sr.reveal("#about,#knowledge,#portfolio,#contact",{
     duration: 2000,
-    origin: "left",
-    distance: "-500px",
-    rotate: {
-        y: 180,
-    },
+    origin: "top",
+    distance: "150px",
     reset: true
 });
-sr.reveal("h1",{
-    duration: 3000,
-    origin: "right",
-    distance: "-200px",
-    reset: true
-});
-sr.reveal("p",{
-    duration: 3000,
-    origin: "left",
-    distance: "-200px",
-    reset: true
-});
+// sr.reveal("h1",{
+//     duration: 2000,
+//     origin: "left",
+//     distance: "-500px",
+//     rotate: {
+//         y: 180,
+//     },
+//     reset: true
+// });
 sr.reveal("figure",{
     duration: 2000,
     rotate: {
